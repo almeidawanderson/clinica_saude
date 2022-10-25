3b72c5b2f9943633c5f9c912b2aabc8db73eee8a
wanderson.almeida <wandersonalmeida.eng@gmail.com>
2022-10-25 15:28:12 -0300

    feat: confiModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,14 @@
-import { Module } from '@nestjs/common';
-import {TypeOrmModule} from '@nestjs/typeorm'
-import {ConfigModule, ConfigService} from '@nestjs/config'
-import { UserModule } from './user/user.module';
-
-
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { SwaggerModule } from "@nestjs/swagger";
+import { DatabaseModule } from "./config/database/database.module";
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('DB_HOST', 'localhost'),
-        port: Number(configService.get('DB_PORT', 3306)),
-        username: configService.get('DB_USERNAME', 'root'),
-        password: configService.get('DB_PASSWORD', 'root'),
-        database: configService.get('DB_DATABASE', 'agendamento_psi'),
-        entities: [],
-        synchronize: true,
-      }),
-    }),
-    UserModule,
-  ],  
-  controllers: [],
-  providers: [],
+    SwaggerModule,
+    ConfigModule,
+    DatabaseModule
+  ],
 })
+
 export class AppModule {}
--- src/user/entities/user.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Column, Entity, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
-
-
-@Entity('USER')
-export class UserEntity {
-    
-
-}
